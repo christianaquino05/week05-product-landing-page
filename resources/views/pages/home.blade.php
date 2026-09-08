b3eddb81a1b3092fe411ce5d15a8de9c147a2ea3
Christian Joseph Aquino <christianjosephpaquino@gmail.com>
2026-09-08 22:20:39 +0800

    chore: initialize Laravel landing page project

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Pinned by ESN</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <h1>Pinned by ESN</h1>
+
+    <p>Pinning your ideas to life.</p>
+
+</body>
+</html>
